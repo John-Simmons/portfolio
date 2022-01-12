e5c906f8a661379c0961e8ff297207a001ffc8af
John Simmons <jsimmons2705@gmail.com>
2022-01-11 21:11:44 -0500

    Changed colors, fixed about spacing

--- src/components/subcomponents/BasicInfo/BasicInfo.js
+++ src/components/subcomponents/BasicInfo/BasicInfo.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom'
 
 export default function BasicInfo(props) {
-  const location = useLocation();
-  let b_info = null;
-
-  if(location.pathname === "/about"){
-    b_info ="I am a detail-oriented product designer, with a background in software engineering, who uses creativity and analytical skill to bring ideas into the real world. As a dedicated lifelong learner, with strong communication skills and a passion for discovering how people think about the world, I seek to solve meaningful problems in thoughtful and insightful ways.";
-  }
-
   return(
     <div className="bi-wrapper">
       <h1 className="bi-name">John Simmons</h1>
       <p className="bi-info subtext">Product Designer @ Persado | NYC</p>
-      <p className="bi-info">{b_info}</p>
     </div>
   )
 }
