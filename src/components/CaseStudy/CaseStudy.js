7109f00701f22b16d5435aeba18287fb1f3e146d
John Simmons <jsimmons2705@gmail.com>
2022-01-13 20:15:54 -0500

    New Logo, more integrated color

--- src/components/CaseStudy/CaseStudy.js
+++ src/components/CaseStudy/CaseStudy.js
@@ -24,7 +24,7 @@ export default function Password() {
           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
 
-        <h2>Section Header</h2>
+        <h2 className="cs-header">Section Header</h2>
 
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
