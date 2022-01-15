cdfdf644c8812a11e93c31ab328453be1852f866
John Simmons <johnsimmons@Johns-MacBook-Pro.local>
2022-01-15 12:04:16 -0500

    Font styling changes

--- src/components/subcomponents/AboutInfo/AboutInfo.js
+++ src/components/subcomponents/AboutInfo/AboutInfo.js
@@ -5,7 +5,7 @@ const aboutInfo = (props) => {
   let aboutInfo = null;
 
   aboutInfo=(
-    <div >
+    <div className="about-info">
       <p className="ai-bio">I am a detail-oriented product designer, with a background in software engineering, who uses creativity and analytical skill to bring ideas into the real world. As a dedicated lifelong learner, with strong communication skills and a passion for discovering how people think about the world, I seek to solve meaningful problems in thoughtful and insightful ways.</p>
       <p className="ai-paragraph">
         <a className="ai-item-wrap" href={resume} target='_blank' rel="noopener noreferrer">
